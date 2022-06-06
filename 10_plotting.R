befa678c85a086d73b591c141c21465c64d0c655
Rodrigo Lima-Lopes <rll307@unicamp.br>
2022-06-06 13:36:08 -0300

    updade scripts

--- 10_plotting.R
+++ 10_plotting.R
@@ -5,6 +5,7 @@ a.tk <- tokens(a.lula_Ciro,
                verbose = TRUE) %>%
   tokens_remove(pattern = stopwords("pt")) %>%
   tokens_group(groups = screen_name)
+writeLines("It is part of my CNPq-funded project and seeks to make corpus tools and R accessible. If you have any doubts or wish to make any research contact please send me an email. Rodrigo de Lima-Lopes rll307@unicamp.br")
 
 dfm.a <- dfm(a.tk, verbose = TRUE)
 
